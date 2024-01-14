80bb3cc20a40824a6a8243c678707b4f92ead8a5
Chris Pinnock <chris_pinnock@mac.com>
2024-01-14 10:05:12 +0000

    tidy up vars

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,43 @@
 # Deploy a Tezos node on GCP
 #
 
-# Externals
+# Disclaimer
 #
-VERS=19.0rc1
-REV=1
-GCLOUDSITE="https://cloud.google.com/sdk/docs/install"
+echo "WARNING: This script will bring up resources on GCP and they will cost money"
+echo "WARNING: Please make sure you understand your budget"
+echo "Ctrl-C now to exit if you are worried - you have 5 seconds"
+sleep 5
+
+# Tezos packaging and snapshotting
+#
 PKGSITE="https://pkgbeta.tzinit.org"
+VERS=19.0rc1
+REV=1 # XXX this should be tunable somewhere
+SNAPREG="eu"
+
+# Tezos network defaults
+#
+NETWORK="mainnet"
+MODE="rolling"
+
+# GCP Defaults
+#
+GCLOUDSITE="https://cloud.google.com/sdk/docs/install"
+MACHINE="e2-standard-4"
+OS=debian-12
+
+# Disc sizes (root currently unused because it is a pain to
+# deal with a second disc)
+#
+DISC_ROOT="20"
+DISC_ROLLING="100"
+DISC_FULL="300"
+DISC_ARCHIVE="2000"
+DISC_SIZE="100" # Default disc size
+
+# External helper script to use
+#
+HELPERSCRIPT="_helper.sh"
 
 warn() {
     echo "$1" >&2
@@ -32,31 +63,16 @@ usage() {
         -o OS      - GCP name for OS (defaults to debian-12)
         -v ver     - version of Octez
         -F         - follow the installation log
-        -c         - CLI options to give to the node
+
     and
         name       - the name of the node (or we construct one)" 
 }
+#         -c         - CLI options to give to the node
+#        -C         - CLOUD (aws, google or azure)
 
-# Defaults
+# Setup
 #
 TMPLOG=`mktemp /tmp/_deployXXXXXX`
-
-HELPERSCRIPT="_helper.sh"
-
-MACHINE="e2-standard-4"
-
-DISC_ROOT="20"
-DISC_ROLLING="100"
-DISC_FULL="300"
-DISC_ARCHIVE="2000"
-SIZE=""
-_SIZE="100"
-
-OS=debian-12
-
-MODE="rolling"
-SNAPREG="eu"
-NETWORK="mainnet"
 
 ZONECLI=""
 PROJECTCLI=""
@@ -75,8 +91,9 @@ which gcloud >/dev/null 2>&1
 #
 while [ $# -gt 0 ]; do
         case $1 in
-        -d)     SIZE="$2"; shift ;;
-        -c)     CLI="$2"; echo "Warning -c not implemented yet"; shift ;;
+        -d)     DISC_SIZE="$2"; shift ;;
+#        -c)     CLI="$2"; echo "Warning -c not implemented yet"; shift ;;
+#        -C)     CLOUD="$2"; echo "Warning -C CLOUD not implented yet"; shift ;;
         -m)     MACHINE="$2"; shift; ;;
         -n)     NETWORK="$2"; shift; ;;
         -o)     OS="$2"; shift; ;;
@@ -114,15 +131,15 @@ esac
 #
 case $MODE in 
     rolling)
-        _SIZE=${DISC_ROLLING};
+        DISC_SIZE=${DISC_ROLLING};
         ;;
 
     full)
-        _SIZE=${DISC_FULL};
+        DISC_SIZE=${DISC_FULL};
         ;;
 
     archive)
-        _SIZE=${DISC_ARCHIVE};
+        DISC_SIZE=${DISC_ARCHIVE};
         ;;
 
     *)
@@ -143,16 +160,18 @@ case $OS in
         ;;
 esac
 
-[ -z "$SIZE" ] && SIZE=$_SIZE
+echo "===> Enabling compute engine"
+gcloud services enable compute.googleapis.com ${PROJECTCLI}
 
 # OS handling
 #
+
 echo "===> Finding OS image for $OS"
 IMAGE=$(gcloud compute images list | grep " $OS " | awk -F' ' '{print $1}')
 [ "$?" != "0" ] && leave "Cannot find a GCP image for $OS"
 echo ${IMAGE}
 
-echo "===> Setting up Google Cloud Node $NAME ($NETWORK/$MODE)"
+echo "===> Setting up Node $NAME ($NETWORK/$MODE)"
 
 gcloud compute instances create ${NAME} \
     ${PROJECTCLI} ${ZONECLI} \
@@ -161,7 +180,7 @@ gcloud compute instances create ${NAME} \
     --maintenance-policy=MIGRATE \
     --provisioning-model=STANDARD \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
-    --create-disk=auto-delete=yes,boot=yes,device-name=${NAME},image=projects/debian-cloud/global/images/${IMAGE},mode=rw,size=${SIZE},\
+    --create-disk=auto-delete=yes,boot=yes,device-name=${NAME},image=projects/debian-cloud/global/images/${IMAGE},mode=rw,size=${DISC_SIZE},\
 type=projects/${PROJECT}/zones/${ZONE}/diskTypes/pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
@@ -185,6 +204,9 @@ rm -f "$TMPLOG"
 
 echo "===> Login with:"
 echo "gcloud compute ssh ${PROJECTCLI} ${ZONECLI} ${NAME}"
+echo "===> Decommission with:"
+echo "gcloud compute instances destroy ${PROJECTCLI} ${ZONECLI} ${NAME}"
+
 
 if [ "$FOLLOW" = "1" ]; then
     echo "===> Following log - it is safe to CTRL-C now if you want to exit"
